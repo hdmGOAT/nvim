fae3754acced02dc8b3ddb6e052df30ea86ce469
Hans Matthew E. Del Mundo <delmundo.hansmatthew@gmail.com>
2025-06-15 17:29:57 +0800

    thank god

--- lua/plugins/lazy.lua
+++ lua/plugins/lazy.lua
@@ -29,7 +29,68 @@ require("lazy").setup({
       })
     end,
   },
+ 
+-- Mason & Go LSP
+{
+  "williamboman/mason.nvim",
+  build = ":MasonUpdate",
+  config = function()
+    require("mason").setup()
+  end,
+},
+{
+  "williamboman/mason-lspconfig.nvim",
+  dependencies = { "williamboman/mason.nvim" },
+  config = function()
+    require("mason-lspconfig").setup({
+      ensure_installed = { "gopls" }, -- ensures gopls gets installed
+    })
+  end,
+},
+{
+  "neovim/nvim-lspconfig",
+  config = function()
+    local lspconfig = require("lspconfig")
 
-  -- LSP + Autocomplete + Go
-  { import = "plugins.go" },  -- points to lua/plugins/go.lua
+    -- Go LSP
+    lspconfig.gopls.setup({
+      settings = {
+        gopls = {
+          analyses = {
+            unusedparams = true,
+            shadow = true,
+          },
+          staticcheck = true,
+        },
+      },
+    })
+  end,
+},
+{
+  "hrsh7th/nvim-cmp",
+  dependencies = {
+    "hrsh7th/cmp-nvim-lsp",
+    "L3MON4D3/LuaSnip",
+  },
+  config = function()
+    local cmp = require("cmp")
+    cmp.setup({
+      snippet = {
+        expand = function(args)
+          require("luasnip").lsp_expand(args.body)
+        end,
+      },
+      mapping = cmp.mapping.preset.insert({
+        ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        ["<Tab>"] = cmp.mapping.select_next_item(),
+        ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+      }),
+      sources = {
+        { name = "nvim_lsp" },
+      },
+    })
+  end,
+}
+
+
 })
