fb14befe970af94372b34d450f4f2b794f987045
Hans Matthew E. Del Mundo <delmundo.hansmatthew@gmail.com>
2025-08-15 15:24:29 +0800

    added typescript and javascript

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -2,6 +2,8 @@
 require("lsp.go")  -- You can add more like `require("lsp.lua")` etc.
 require("lsp.rust")
 require("lsp.python")
+require("lsp.js_ts")
+
 
 vim.diagnostic.config({
   virtual_text = false,
---
+++ lua/lsp/js_ts.lua
@@ -0,0 +1,34 @@
+-- lua/lsp/js_ts.lua
+local lspconfig = require("lspconfig")
+local cmp_nvim_lsp = require("cmp_nvim_lsp")
+
+-- Optional: Add capabilities for better completion support
+local capabilities = cmp_nvim_lsp.default_capabilities()
+
+lspconfig.tsserver.setup({
+  capabilities = capabilities,
+  settings = {
+    typescript = {
+      inlayHints = {
+        includeInlayParameterNameHints = "all",
+        includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+        includeInlayFunctionParameterTypeHints = true,
+        includeInlayVariableTypeHints = true,
+        includeInlayPropertyDeclarationTypeHints = true,
+        includeInlayFunctionLikeReturnTypeHints = true,
+        includeInlayEnumMemberValueHints = true,
+      },
+    },
+    javascript = {
+      inlayHints = {
+        includeInlayParameterNameHints = "all",
+        includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+        includeInlayFunctionParameterTypeHints = true,
+        includeInlayVariableTypeHints = true,
+        includeInlayPropertyDeclarationTypeHints = true,
+        includeInlayFunctionLikeReturnTypeHints = true,
+        includeInlayEnumMemberValueHints = true,
+      },
+    },
+  },
+})
