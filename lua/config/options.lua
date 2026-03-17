8de8d18183b30c8447f38a26ea9ff1dbdfbc391c
Hans Matthew E. Del Mundo <delmundo.hansmatthew@gmail.com>
2026-03-17 10:54:38 +0800

    fix tabs

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,5 +1,9 @@
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.termguicolors = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.expandtab = false
 
 vim.cmd.colorscheme("tokyonight")
